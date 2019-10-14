9517505c3ac8c3e94239c511d0ef81cff873a7d8
IbarraTefi <54083754+IbarraTefi@users.noreply.github.com>
2019-10-13 23:26:52 -0300

    Entrega2 completa

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
-let productUnitCost = 0;
+
+let productCount = 0;
 let productCurrency = "";
-var currentArticlesArray = [];
 let subtotal = 0;
 let shippingPercentage = 0.15;
 let total = 0;
@@ -11,11 +11,16 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
-
+    
 }
 
 function updateSubtotal(){
 
+let subTotalHTML = document.getElementById("subTotal");
+
+let subTotalCostToShow = (productUnitCost * productCount);
+
+subTotalHTML.innerHTML = subTotalCostToShow;
     
 }
 
@@ -31,21 +36,40 @@ function showArticles(articles){
     let htmlContentToAppend = "";
 
     for(let i = 0; i < articles.length; i++){
-        let article = articles[i];
+        let articulo = articles[i];
 
         htmlContentToAppend += `
-        <div class="row">
-            <div class="col-3">
-                <img src="` + article.src + `" alt="` + article.name + ` class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">` + article.name +`</h4>
-                     <small class="text-muted">` + article.currency + " " + article.unitCost + `</small>
-                </div>
-                <p class="mb-1">` + article.count + `</p>
-            </div>
-        </div>
+        <table border="1">
+        <tbody>
+        <tr>
+        <td align="center">
+        <h5> <strong>Producto<strong></h5>
+        </td>
+        <td align="center">
+        <h5><strong>Nombre<strong></h5>
+        </td>
+        <td align="center">
+        <h5><strong>Stock<strong></h5>
+        </td>
+        <td align="center">
+        <h5><strong>Precio unitario<strong></h5>
+        </td>
+        </tr>
+        <tr>
+        <td align="center">
+                <img src="` + articulo.src + `" alt="` + articulo.name + ` class="img-thumbnail" width="30%">
+        </td>
+        <td align="center">
+                        <h6 class="mb-1">` + articulo.name +`</h6>
+        </td>
+        <td align="center">  
+                <h6 class="mb-1" id="stock">` + articulo.count + `</h6>
+        </td>
+        <td align="center">
+            <h6 class="mb-1" id="precio">` + articulo.unitCost + `</h6> 
+        </td>
+        </tbody>
+        </table>
         `
     }
     document.getElementById("art-list-container").innerHTML = htmlContentToAppend;
@@ -59,8 +83,17 @@ document.addEventListener("DOMContentLoaded", function(e){
         if(resultObj.status === "ok"){
 
             infoArticle = resultObj.data.articles;
+            productUnitCost = infoArticle[0].unitCost;
 
             showArticles(infoArticle);
+
         }
     });
+
+    document.getElementById("articleCountInput").addEventListener("change", function(){
+        productCount = this.value;
+        
+        updateSubtotal();
+    });
+
 });
